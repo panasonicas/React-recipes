b0ec64d366b6c8b8045f5ec4ff720a3ba732b705
panasonicas <58065520+panasonicas@users.noreply.github.com>
2020-05-18 13:23:08 +0300

    Request from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+const APP_ID = '63fe98ad';
+const APP_KEY = 'ce22b92d5c6d31c0a380df7e508daa01';
+const [recepies, setRecepies] = useState([]);
+
+
+useEffect(()=>{
+  getRecepies();
+},[]);
+
+const getRecepies = async () => {
+  const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  const data = await response.json();
+  setRecepies(data.hits);
+};
+
+  return(
+  <div className="App">
+    <form className="search-form">
+      <input className ="search-bar" type="text"/>
+      <button className ="search-button" type="submit">Search</button>
+    </form>
+  </div>
   );
 }
 
